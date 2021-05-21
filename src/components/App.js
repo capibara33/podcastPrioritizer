15cfbc6424123af50aacef424f5faa206bf36998
Aubrey Kazdan <aubrey.j.kazdan@gmail.com>
2021-05-20 22:27:39 -0400

    added skeleton html

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,9 @@
 import { Fragment } from 'react';
 import '../styles/App.css';
+import LandingPage from './LandingPage';
 import LocationForm from './LocationForm';
+import PodcastSearch from './PodcastSearch';
+import PodcastResults from './PodcastResults';
 
 function App() {
   return (
@@ -8,8 +11,10 @@ function App() {
       
 
 
-
+      <LandingPage />
       <LocationForm />
+      <PodcastSearch />
+      <PodcastResults />
 
     </Fragment>
   );
--- src/components/PodcastResults.js
+++ src/components/PodcastResults.js
@@ -1,18 +1,20 @@
 
 const PodcastResults = () => {
-    <div className="podcastResults">
-        <h2>List of Podcast</h2>
-        <ul>
-        {/* {
-            podcasts.map((podcast)=>{
-                return 
-                <li>
-                
-                </li>
-            })
-        } */}
-        </ul>
-    </div>
+    return (
+        <div className="podcastResults">
+            <h2>List of Podcasts</h2>
+            <ul>
+            {/* {
+                podcasts.map((podcast)=>{
+                    return 
+                    <li>
+                    
+                    </li>
+                })
+            } */}
+            </ul>
+        </div>
+    )
 }
 
 export default PodcastResults
