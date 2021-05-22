bb89bf383f748d65171778292724ab873770521d
Clement Sung <clement.sung7@gmail.com>
2021-05-22 11:46:20 -0400

    added podcast search logic

--- src/components/PodcastResults.js
+++ src/components/PodcastResults.js
@@ -1,23 +1,33 @@
 
-const PodcastResults = () => {
+const PodcastResults = (props) => {
+    const {podcastInfo} = props
+
+
     return (
         <section className="podcastResults">
             <div className="podcastResultsContainer">
                 <h2>List of Podcasts</h2>
                 <ul>
-                {/* {
-                    podcasts.map((podcast)=>{
-                        return 
-                        <li>
-                        
-                        </li>
+                {
+                        podcastInfo.map(({ podcastTitle, episodeTitle, episodeImage, episodeDescription, episodeAudio}, index)=>{
+                        return(
+                            <li key={index}>
+                                <h2>{podcastTitle}</h2>
+                                <h3>{episodeTitle}</h3>
+                                <img src={episodeImage} alt="" />
+                                <p>{episodeDescription}</p>
+                                <audio controls> 
+                                    <source src={episodeAudio} type="audio/mpeg"/>
+                                </audio>
+                            </li>
+                        ) 
                     })
-                } */}
+                }
+                    {/* <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis, a?</li>
                     <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis, a?</li>
                     <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis, a?</li>
                     <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis, a?</li>
-                    <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis, a?</li>
-                    <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis, a?</li>
+                    <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis, a?</li> */}
                 </ul>
             </div> 
         </section>
--- src/components/PodcastSearch.js
+++ src/components/PodcastSearch.js
@@ -1,13 +1,60 @@
-const PodcastSearch = () => {
+import { FaArrowCircleRight } from 'react-icons/fa';
+import { useState, useEffect } from 'react'
 
+const PodcastSearch = (props) => {
+	const podcastURL = new URL('https://listen-api.listennotes.com/api/v2/search')
+	const { handlePodcastData } = props
+	const [userSearch, setUserSearch] = useState('');
 
+	const podcastFetch = (() => {
+		
+	podcastURL.search = new URLSearchParams({
+		q: userSearch,
+		len_min: 0,
+		len_max: 180
+	})
+
+	fetch(podcastURL, {
+		method: "GET",
+		headers: { 'X-ListenAPI-Key': '293a53ed0c034ba8b5384c3905a13cc1' }
+	})
+		.then((data) => {
+			return data.json();
+		})
+		.then((jsonResponse) => {
+			
+			const podcastData = jsonResponse.results
+			const podcastArray = podcastData.map((podcast) => {
+				return {
+					podcastTitle:podcast.title_original,
+					episodeTitle:podcast.podcast_title_original,
+					episodeImage:podcast.image,
+					episodeDescription:podcast.description_original,
+					episodeAudio:podcast.audio
+				}
+			})
+			console.log(podcastArray)
+			handlePodcastData(podcastArray)
+			
+		})
+	})
+	
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		podcastFetch();
+	}
+
+	const handlePodcastSearch = (event) => {
+		setUserSearch(event.target.value)
+	}
 
 
 	return(
 		<div>
-			<form action="">
+			<form action="" onSubmit={handleSubmit}>
 				<label htmlFor="podcastSearch" className="srOnly"></label>
-				<input type="text" id="podcastSearch" name="podcastSearch"/>
+				<input type="text" id="podcastSearch" name="podcastSearch" onChange={handlePodcastSearch} value={userSearch}/>
+				<FaArrowCircleRight />
 				<button>Random Podcast</button>
 			</form>
 		</div>
