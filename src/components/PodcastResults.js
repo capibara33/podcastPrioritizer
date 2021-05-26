17bf704a1f3eb917298582616d4809aa75ac946b
Aubrey Kazdan <aubrey.j.kazdan@gmail.com>
2021-05-26 12:00:44 -0400

    all that for a disappearing podcast list

--- src/components/PodcastResults.js
+++ src/components/PodcastResults.js
@@ -8,43 +8,41 @@ const PodcastResults = (props) => {
 
     return (
         <section className="podcastResults">
-            <div className="wrapper podcastResultsContainer">
-                <h2>List of Podcasts</h2>
-                <ul className="podcastsList">
                 {
-                        
-                        podcastInfo.map(({ podcastTitle, episodeTitle, episodeImage, episodeDescription, episodeLengthInSec, episodeAudio}, index)=>{
+                    podcastInfo ? podcastInfo.map(({ podcastTitle, episodeTitle, episodeImage, episodeDescription, episodeLengthInSec, episodeAudio}, index)=>{
                         return(
-                            <li key={index}>
-                                <div className="liContainer">
-                                    <div className="podcastTitleContainer">
-                                        <img src={episodeImage} alt="" />
-                                        <h3 className="resultsTitle">{podcastTitle}</h3>
+                        <div className="wrapper podcastResultsContainer">
+                            <h2>List of Podcasts</h2>
+                            <ul className="podcastsList">
+                                <li key={index}>
+                                    <div className="liContainer">
+                                        <div className="podcastTitleContainer">
+                                            <img src={episodeImage} alt="" />
+                                            <h3 className="resultsTitle">{podcastTitle}</h3>
+                                        </div>
+                                        <p>Episode title: {episodeTitle}</p>
+                                        <p>Length: {timeConverter(episodeLengthInSec)}</p>
+                                        <div className="podcastInfoContainer">
+                                            <button aria-label={`Description for ${podcastTitle}`} className="podcastInfoButton" onClick={() => {
+                                                Swal.fire({
+                                                    title: podcastTitle,
+                                                    text: episodeDescription,
+                                                    time: episodeLengthInSec,
+                                                    confirmButtonText: "Return",
+                                                    padding:"10px"
+                                                })
+                                            }
+                                        }>Learn More</button>
+                                    <a aria-label={`External link to audio of ${episodeTitle}`} target="_blank" href={episodeAudio} rel="noreferrer">Listen Here <FiExternalLink /></a>
+                                        </div>
                                     </div>
-                                    <p>Episode title: {episodeTitle}</p>
-                                    <p>Length: {timeConverter(episodeLengthInSec)}</p>
-                                    <div className="podcastInfoContainer">
-                                        <button aria-label={`Description for ${podcastTitle}`} className="podcastInfoButton" onClick={() => {
-                                            Swal.fire({
-                                                title: podcastTitle,
-                                                text: episodeDescription,
-                                                time: episodeLengthInSec,
-                                                confirmButtonText: "Return",
-                                                padding:"10px"
-                                            })
-                                        }
-                                    }>Learn More</button>
-                                <a aria-label={`External link to audio of ${episodeTitle}`} target="_blank" href={episodeAudio} rel="noreferrer">Listen Here <FiExternalLink /> </a>
-                                    
-                                    </div>
-                                </div>
-                            </li>
+                                </li>
+                            </ul>
+                    </div> 
                         ) 
                     })
-                    
+                    : <p></p>
                 }
-                </ul>
-            </div> 
         </section>
     )
 }
--- src/components/PodcastSearch.js
+++ src/components/PodcastSearch.js
@@ -3,37 +3,30 @@ import { useState } from 'react'
 
 const PodcastSearch = (props) => {
 	const podcastURL = new URL('https://listen-api.listennotes.com/api/v2/search')
-	const { handlePodcastData, commuteTime } = props
+	const podcastKey = '293a53ed0c034ba8b5384c3905a13cc1';
 	const [userSearch, setUserSearch] = useState('');
+	const { handlePodcastData, commuteTime } = props;
 	
-	const commuteInMinutes = Math.floor(commuteTime / 60)
+	const commuteInMinutes = Math.floor(commuteTime / 60);
 	
 	const suggestedUserSearch = ["music", "movies", "art", "dance", "programming", "ants", "travel", "news", "sports", "food", "paranormal", "books", "true crime", "tv", "comedy", "video games", "aliens"]
 
-	// console.log(commuteInMinutes + 10)
-
 	const podcastFetch = ((query) => {
-	
-	
-	podcastURL.search = new URLSearchParams({
-		q: query,
-		len_min: commuteInMinutes,
-		len_max: commuteInMinutes + 10,
-	})
-
-	fetch(podcastURL, {
-		method: "GET",
-		headers: { 'X-ListenAPI-Key': '293a53ed0c034ba8b5384c3905a13cc1' }
-	})
+		podcastURL.search = new URLSearchParams({
+			q: query,
+			len_min: commuteInMinutes,
+			len_max: commuteInMinutes + 10,
+		})
+		fetch(podcastURL, {
+			method: "GET",
+			headers: { 'X-ListenAPI-Key': podcastKey }
+		})	
 		.then((data) => {
 			return data.json();
 		})
 		.then((jsonResponse) => {
-			
 			const podcastData = jsonResponse.results
-			console.log(podcastData)
 			const podcastArray = podcastData.map((podcast) => {
-
 				return {
 					podcastTitle:podcast.podcast_title_original,
 					episodeTitle:podcast.title_original,
@@ -43,8 +36,7 @@ const PodcastSearch = (props) => {
 					episodeLengthInSec:podcast.audio_length_sec
 				}
 			})
-			handlePodcastData(podcastArray)
-			
+		handlePodcastData(podcastArray)
 		})
 	})
 
