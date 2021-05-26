a8700be641e56d9b72d4c4969bbbc22ae81d8786
Ilya-Marvin <ilya44.m@gmail.com>
2021-05-25 22:00:14 -0400

    working-giphy-api

---
+++ src/components/Giphy.js
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react'
+
+const Giphy = () => {
+
+    const [giph, setGiph] = useState([]);
+    
+    const giphyUrl = new URL(`https://api.giphy.com/v1/gifs`);
+
+    useEffect(()=>{
+        giphyUrl.search = new URLSearchParams({
+            api_key: 'HVo4BiCuwH7vyhWSzlAfRqhIp06cIt7O',
+            ids: 'XeGljuPcbTPtfEwQVo, gFfd9ZgyukhImSYwPa, poqFvVAbhzqwrYqfSG,cIhkizr8QLITdoWSzC,ifGVZZ9QEEMs1ia0OJ,s9vFiYCdomuycLyT1X'
+        });
+    
+        fetch(giphyUrl)
+            .then((giphyData) => {
+                return giphyData.json();
+            })
+            .then((jsonGiphyResponse) => {
+                const giphyArray = (jsonGiphyResponse.data);
+    
+                    const randomGiph = giphyArray[Math.floor(Math.random() * giphyArray.length)];
+
+                    setGiph(randomGiph);
+            });
+            
+        }, []);        
+
+    let giphySrc
+    let giphyAlt
+
+    if (giph.images && giph.images.original) {
+        giphySrc = giph.images.original.url
+        giphyAlt = giph.title
+    };
+
+    return (
+        <>
+            <img className="giph" src={giphySrc} alt={giphyAlt} />
+        </>
+    );
+}
+
+export default Giphy
