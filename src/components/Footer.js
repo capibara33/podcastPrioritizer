71e947b83d88b38f1674478457b84d5c49645938
Aubrey Kazdan <aubrey.j.kazdan@gmail.com>
2021-05-27 16:59:06 -0400

    added fade-in and icon to step 1

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,16 @@
+import { animateScroll as scroll } from 'react-scroll';
+
+
 const Footer = () => {
+  const toTop = () => {
+    scroll.scrollToTop();
+  }
+  
   return(
     <footer>
+      <div className="wrapper scrollButtonContainer">
+          <button className="scrollToTop" onClick={toTop}>Scroll to top</button>
+      </div>
       <p className="githubLinks">
         Team CAPIbara is:
         <a href="https://github.com/clembrulee"> Clement Sung</a>,
@@ -11,6 +21,7 @@ const Footer = () => {
       </p>
       <p>Made @ <a href="https://junocollege.com/">Juno College </a>2021</p>
       <div>Favicon made by <a href="https://www.flaticon.com/authors/dinosoftlabs" title="DinosoftLabs">DinosoftLabs</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+      <div>Mapquest API provided by <a href="https://developer.mapquest.com/">Mapquest</a> </div>
       <img src={process.env.PUBLIC_URL + 'listennotesLogo.png'} alt="powered by listen notes logo" className="listennotesLogo" />
     </footer>
   )
