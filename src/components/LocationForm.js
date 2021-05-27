059bdacc94cf647523621aca920d07a04d23f7d4
PaulSzadurski <77594751+PaulSzadurski@users.noreply.github.com>
2021-05-27 15:59:19 -0400

    Update LocationForm.js

--- src/components/LocationForm.js
+++ src/components/LocationForm.js
@@ -129,7 +129,7 @@ const noRoute = () => {
               <label htmlFor="currentLocation">Current Location:</label>
               <input  placeholder="483 Queen St W Toronto" required type="text" id="currentLocation" onChange={handleLocationInput} value={location}></input>
 
-              <button className="myLocation" onClick={() => { myLocation() }}><BiCurrentLocation /></button>
+              <button type="button" className="myLocation" onClick={() => { myLocation() }}><BiCurrentLocation /></button>
             </div>
 
             <label htmlFor="destination">Your Destination:</label>
